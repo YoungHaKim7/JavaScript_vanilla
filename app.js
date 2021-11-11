fbef06eb9ece139a5c3a9536755dda96435d0ca1
YoungHa Kim <ytok1108@aol.com>
2021-11-11 21:59:10 +0900

    4.7 another method

--- app.js
+++ app.js
@@ -8,23 +8,21 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello  ${username}`;
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello! :)  ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-console.log(savedUsername);
-
 if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 }else {
-    paintGreetings(savedUsername);
+    paintGreetings();
 }
