78338712409df79aaf74933d2a30751a2a9e2ee2
YoungHa Kim <ytok1108@aol.com>
2021-11-13 14:19:58 +0900

    8.2 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,24 @@
+
+
+const API_KEY = "94b3980686a01ce18ddc81a3408aa336";
+
+
+
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
-
+    const log = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp} ℃`;
+    });
 }
+
 
 function onGeoError() {
     alert("Can't find you. No weather for you.");
